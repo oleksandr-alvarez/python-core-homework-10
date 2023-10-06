22d184f245d3431596fb22d2755775eedae1262b
Oleksandr Alvarez <alexander_1@onmail.com>
2023-10-05 22:42:29 -0400

    all tests are passed

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ class Phone(Field):
     def __init__(self, value):
         super().__init__(value)
         if len(self.value) != 10:
-            raise PhoneTooShort
+            raise  PhoneTooShort("Phone number should be 10 digits long.")
         if len(re.findall('\d', self.value)) != 10:
-            raise PhoneIncludesNotOnlyNumbers
+            raise PhoneIncludesNotOnlyNumbers("Phone number should only include digits.")
     
 
 class Record:
@@ -38,21 +38,26 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone):
-        self.phones.append(Phone(phone).value)
+        self.phones.append(Phone(phone))
     
     def remove_phone(self, phone):
-        self.phones.remove(Phone(phone).value)
+         list_of_phones = [el.value for el in self.phones]
+         if phone in list_of_phones:
+            index_of_phone_to_remove = list_of_phones.index(phone)
+            self.phones.pop(index_of_phone_to_remove)
 
     def edit_phone(self, old_phone, new_phone):
-        self.phones[self.phones.index(old_phone)] = new_phone
+        list_of_phones = [el.value for el in self.phones]
+        self.phones[list_of_phones.index(old_phone)] = Phone(new_phone)
     
     def find_phone(self, phone_to_find):
-        if phone_to_find in self.phones:
-            return phone_to_find
+        list_of_phones = [el.value for el in self.phones]
+        if phone_to_find in list_of_phones:
+            return self.phones[list_of_phones.index(phone_to_find)]
 
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
     def add_record(self, record):
@@ -62,8 +67,8 @@ class AddressBook(UserDict):
         if name_value in self.data:
             found_record = self.data[name_value]
             return found_record
-        else:
-            return None
+        return None
     
     def delete(self, name):
-        self.data.pop(name)
+        if name in self.data:
+            self.data.pop(name)
